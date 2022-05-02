14f73fd5e2a9329184ed5c40d997d370177cbb4d
barret <barret@tarkmail.com>
2022-05-02 18:50:18 -0500

    Set main test level to trace and add tests for all levels

--- tests/test_get_logger.py
+++ tests/test_get_logger.py
@@ -13,6 +13,12 @@ class TestGetLogger(unittest.TestCase):
     def test_with_file(self):
         """Tests instantiation without setting up a file sink"""
         mock_path = pathlib.Path(tempfile.gettempdir())
-        log = get_logger(log_name='test', log_dir_path=mock_path)
+        log = get_logger(log_name='test', log_dir_path=mock_path, base_level='TRACE')
+        log.trace('Test trace')
+        log.debug('Test debug')
+        log.info('Test info')
+        log.warning('Test warning')
+        log.error('Test error')
+        log.critical('Test critical')
         self.assertEqual(2, len(log._core.handlers.keys()))
         self.assertTrue(mock_path.joinpath('test.log').exists())
